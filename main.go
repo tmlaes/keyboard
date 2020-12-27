9ee22f9ff3eb828f1063a42f1831fe104af483b8
superman <wangzezheng1991@163.com>
2020-12-27 14:03:29 +0800

    简化

--- main.go
+++ main.go
@@ -10,12 +10,7 @@ import (
 )
 
 const (
-	// 2 20 5 21 7
-	time1 = 1333 * time.Millisecond
-	time2 = 20333 * time.Millisecond
-	time3 = 5333 * time.Millisecond
-	time5 = 21333 * time.Millisecond
-	time6 = 7333 * time.Millisecond
+	time1 = 700 * time.Millisecond
 )
 
 var (
@@ -24,15 +19,7 @@ var (
 	//1：暂停；0：运行
 	pause    int32
 	skill1Ch = make(chan int, 1)
-	skill2Ch = make(chan int, 1)
-	skill3Ch = make(chan int, 1)
-	skill5Ch = make(chan int, 1)
-	skill6Ch = make(chan int, 1)
 	ticker1  = time.NewTicker(time1)
-	ticker2  = time.NewTicker(time2)
-	ticker3  = time.NewTicker(time3)
-	ticker5  = time.NewTicker(time5)
-	ticker6  = time.NewTicker(time6)
 )
 
 func main() {
@@ -45,17 +32,17 @@ func command() {
 	for {
 		time.Sleep(time.Duration(randDelay()) * time.Millisecond)
 		//开始
-		if windev.KeyDownUp(windev.VK_CHARZ) == 1 {
+		if windev.KeyDownUp(windev.VK_CHARF) == 1 {
 			reset()
 			atomic.CompareAndSwapInt32(&pause, 0, 1)
 		}
 		//暂停
-		if windev.KeyDownUp(windev.VK_CHARX) == 1 {
+		if windev.KeyDownUp(windev.VK_CHARG) == 1 {
 			stop()
 			atomic.CompareAndSwapInt32(&pause, 1, 0)
 		}
 		//结束
-		if windev.KeyDownUp(windev.VK_CHARB) == 1 {
+		if windev.KeyDownUp(windev.VK_F1) == 1 {
 			exit()
 		}
 	}
@@ -65,7 +52,6 @@ func run() {
 	defer exit()
 	keyProc, _ := DD.FindProc("DD_key")
 	mouseProc, _ := DD.FindProc("DD_btn")
-	skill5Ch <- 0
 	for {
 		time.Sleep(time.Duration(randDelay()) * time.Millisecond)
 		if atomic.LoadInt32(&pause) == 0 {
@@ -74,16 +60,8 @@ func run() {
 		select {
 		case <-skill1Ch:
 			skill1(keyProc)
-		case <-skill2Ch:
-			skill2(keyProc)
-		case <-skill3Ch:
-			skill3(keyProc)
-		case <-skill5Ch:
-			skill5(keyProc, mouseProc)
-		case <-skill6Ch:
-			skill6(keyProc)
 		default:
-			skill4(keyProc)
+			skill2(keyProc, mouseProc)
 		}
 	}
 }
@@ -96,32 +74,8 @@ func skill1(keyProc *syscall.Proc) {
 	randDelayMin()
 }
 
-//数字2
-func skill2(keyProc *syscall.Proc) {
-	keyProc.Call(uintptr(202), 1)
-	randDelayMin()
-	keyProc.Call(uintptr(202), 2)
-	randDelayMin()
-}
-
-//数字3
-func skill3(keyProc *syscall.Proc) {
-	keyProc.Call(uintptr(203), 1)
-	randDelayMin()
-	keyProc.Call(uintptr(203), 2)
-	randDelayMin()
-}
-
-//数字4
-func skill4(keyProc *syscall.Proc) {
-	keyProc.Call(uintptr(204), 1)
-	randDelayMin()
-	keyProc.Call(uintptr(204), 2)
-	randDelayMin()
-}
-
 //shift+鼠标左键
-func skill5(keyProc *syscall.Proc, mouseProc *syscall.Proc) {
+func skill2(keyProc *syscall.Proc, mouseProc *syscall.Proc) {
 	keyProc.Call(uintptr(500), 1)
 	randDelayMin()
 	mouseProc.Call(uintptr(1))
@@ -132,14 +86,6 @@ func skill5(keyProc *syscall.Proc, mouseProc *syscall.Proc) {
 	randDelayMin()
 }
 
-//鼠标右键
-func skill6(mouseProc *syscall.Proc) {
-	mouseProc.Call(uintptr(4))
-	randDelayMin()
-	mouseProc.Call(uintptr(8))
-	randDelayMin()
-}
-
 //定时任务
 func ticker() {
 	go func() {
@@ -147,46 +93,16 @@ func ticker() {
 			skill1Ch <- 0
 		}
 	}()
-	go func() {
-		for range ticker2.C {
-			skill2Ch <- 0
-		}
-	}()
-
-	go func() {
-		for range ticker3.C {
-			skill3Ch <- 0
-		}
-	}()
-
-	go func() {
-		for range ticker5.C {
-			skill5Ch <- 0
-		}
-	}()
-	go func() {
-		for range ticker6.C {
-			skill6Ch <- 0
-		}
-	}()
 }
 
 //重置定时器
 func reset() {
 	ticker1.Reset(time1)
-	ticker2.Reset(time2)
-	ticker3.Reset(time3)
-	ticker5.Reset(time5)
-	ticker6.Reset(time6)
 }
 
 //暂停取消定时器
 func stop() {
 	ticker1.Stop()
-	ticker2.Stop()
-	ticker3.Stop()
-	ticker5.Stop()
-	ticker6.Stop()
 }
 
 //退出释放资源
@@ -199,7 +115,7 @@ func exit() {
 //随机延迟时间
 func randDelay() int {
 	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(60) + 60
+	return rand.Intn(80) + 80
 }
 
 //随机延迟时间
